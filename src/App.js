5cded217dc53903b3d6c11c9feae3a1a731d288c
Andre Almeida <andre-silva78@hotmail.com>
2021-01-06 18:31:52 -0300

    ++ hero section created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 import GlobalStyle from "./globalStyles";
+import sliderData from "./data/SliderData";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Navbar />
+      <Hero slides={sliderData} />
     </div>
   );
 }
---
+++ src/data/SliderData.js
@@ -0,0 +1,32 @@
+import ImageOne from "../images/allianz.png";
+import ImageTwo from "../images/primeiropay.png";
+import ImageThree from "../images/clarice.png";
+
+export const sliderData = [
+  {
+    title: "Projeto Allianz Parque",
+    data: "2019",
+    label: "Ver projeto",
+    path: "/",
+    image: ImageOne,
+    alt: "Projeto Allianz Parque",
+  },
+  {
+    title: "Projeto PrimeiroPay",
+    data: "2019",
+    label: "Ver projeto",
+    path: "/",
+    image: ImageTwo,
+    alt: "Projeto PrimeiroPay",
+  },
+  {
+    title: "Projeto Clarice Lispector",
+    data: "2020",
+    label: "Ver projeto",
+    path: "/",
+    image: ImageThree,
+    alt: "Projeto Clarice Lispector",
+  },
+];
+
+export default sliderData;
