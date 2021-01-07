03d744ea354ee3ab190f7d284e95a8b716e73c60
Andre Almeida <andre-silva78@hotmail.com>
2021-01-06 21:43:40 -0300

    ++ slider function on hero section with useState/useRef

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled, { css } from "styled-components/macro";
 import { sliderData } from "../data/SliderData";
 import { Button } from "./Button";
@@ -46,7 +46,7 @@ const HeroSlider = styled.div`
     bottom: 0vh;
     left: 0;
     overflow: hidden;
-    opacity: 0.3;
+    opacity: 0.5;
     background: linear-gradient(
       0deg,
       rgba(0, 0, 0, 1) 0%,
@@ -72,10 +72,12 @@ const HeroContent = styled.div`
   flex-direction: column;
   max-width: 1600px;
   width: calc(100%-100px);
+  margin-left: 10%;
+  margin-right: 10%;
   color: #fff;
 
   h1 {
-    font-size: clamp(1rem, 8vh, 20rem);
+    font-size: clamp(3rem, 8vw, 2rem);
     font-weight: 400;
     text-transform: uppercase;
     text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
@@ -89,7 +91,9 @@ const HeroContent = styled.div`
   }
 `;
 
-const Arrow = styled(IoMdArrowForward)``;
+const Arrow = styled(IoMdArrowForward)`
+  margin-left: 0.5rem;
+`;
 
 const ArrowButtons = css`
   width: 50px;
@@ -126,35 +130,73 @@ const NextArrow = styled(IoMdArrowForward)`
 `;
 
 const Hero = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+  const timeout = useRef(null);
+
+  useEffect(() => {
+    const nextSlide = () => {
+      setCurrent((current) => (current === length - 1 ? 0 : current + 1));
+    };
+
+    timeout.current = setTimeout(nextSlide, 5000);
+
+    return function () {
+      if (timeout.current) {
+        clearTimeout(timeout.current);
+      }
+    };
+  }, [current, length]);
+
+  const nextSlide = () => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+    setCurrent(current === 0 ? length - 1 : current + 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
   return (
     <HeroSection>
       <HeroWrapper>
         {slides.map((slide, index) => {
           return (
             <HeroSlide key={index}>
-              <HeroSlider>
-                <HeroImage src={slide.image} alt={slide.alt} />
-                <HeroContent>
-                  <h1>{slide.title}</h1>
-                  <p>{slide.data}</p>
-                  <Button
-                    to={slide.path}
-                    primary="true"
-                    css={`
-                      max-width: 160px;
-                    `}
-                  >
-                    {slide.label}
-                    {/* <Arrow /> */}
-                  </Button>
-                </HeroContent>
-              </HeroSlider>
+              {index === current && (
+                <HeroSlider>
+                  <HeroImage src={slide.image} alt={slide.alt} />
+                  <HeroContent>
+                    <h1>{slide.title}</h1>
+                    <p>{slide.data}</p>
+                    <Button
+                      to={slide.path}
+                      primary="true"
+                      css={`
+                        max-width: 160px;
+                      `}
+                    >
+                      {slide.label}
+                      {/* <Arrow /> */}
+                    </Button>
+                  </HeroContent>
+                </HeroSlider>
+              )}
             </HeroSlide>
           );
         })}
         <SliderNavigation>
-          <PrevArrow />
-          <NextArrow />
+          <PrevArrow onClick={prevSlide} />
+          <NextArrow onClick={nextSlide} />
         </SliderNavigation>
       </HeroWrapper>
     </HeroSection>
